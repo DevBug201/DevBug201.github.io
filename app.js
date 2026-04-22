3e3278d90643581ad116527a2f9e1e4ffdfd4913
DevBug <carlosestudiantedaw@gmail.com>
2026-04-22 10:19:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -128,5 +128,4 @@ if (terminalScreen) terminalScreen.addEventListener('click', () => termInput.foc
 if (termInputRow) termInputRow.addEventListener('click', () => termInput.focus());
 
 addLines(commands.help, 'help');
-termInput.focus({ preventScroll: true });
 updateCursorPosition();
